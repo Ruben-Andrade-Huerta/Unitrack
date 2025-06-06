c0e2dc49ab421c2b02fb9750780a1e2f35c464fb
Ruben-Andrade-Huerta <rubsan0503@gmail.com>
2025-06-06 15:15:00 -0600

    install psycopg2-binary

--- unitrack_backend/unitrack_backend/settings.py
+++ unitrack_backend/unitrack_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,18 +80,7 @@ WSGI_APPLICATION = 'unitrack_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'unitrack_app',
-        'USER': 'rubenandrade',  # Replace with your database user
-        'PASSWORD': 'nueva_contraseña',  # Replace with your database password
-        'HOST': 'localhost',  # Replace with your database host
-        'PORT': '5432',  # Replace with your database port
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
